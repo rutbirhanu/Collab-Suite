4489059096e33f8b66f1068f54e52908a0bdba59
Rut Birhanu <birhanurut@gmail.com>
2025-12-08 22:42:53 +0300

    implement user registration and login

--- apps/auth-service/src/auth/auth.module.ts
+++ apps/auth-service/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: []
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 
 export class AuthModule {}
--- apps/auth-service/src/auth/auth.service.ts
+++ apps/auth-service/src/auth/auth.service.ts
@@ -1,4 +1,50 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import * as bcrypt from "bcryptjs"
 
-export class AuthService{
-    
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async register(email: string, password: string, fullName: string) {
+        const existingUser = await this.prisma.user.findUnique({ where: { email } })
+
+        if (existingUser) {
+            throw new BadRequestException("User with this email already exists")
+        }
+
+        const hashed = await bcrypt.hash(password, 10)
+        const user = await this.prisma.user.create({
+            data: {
+                email, password: hashed, fullName
+            }
+        })
+
+        return {id: user.id, email: user.email, fullName: user.fullName}
+
+    }
+
+
+    async login(email: string, password: string) {
+        const user = await this.prisma.user.findUnique({ where: { email } })
+        if (!user) {
+            return null
+        }
+        const valid = await bcrypt.compare(password, user.password)
+        if (!valid) {
+            return null
+        }
+        return user
+    }
+
+
+
+    async validateUser(email: string, password: string) {
+
+    }
+
+
+    async validateToken() {
+        
+    }
 }
